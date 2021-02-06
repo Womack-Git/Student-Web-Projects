2fcb5f582fbd01195d1e85f920f189081491b0c7
awomack333 <awomack333@gmail.com>
2021-02-06 00:17:48 +0000

    Change Mario Project 1 to Mario 2

--- Web Scripting/Mario Project/mario.js
+++ Web Scripting/Mario Project/mario.js
@@ -2,15 +2,24 @@ let height = prompt("How many blocks tall should the pyramid be?");
 
 function printPyramid(rows, height){
   let str = '';
-  for(let spaces = height - rows; spaces>=0; spaces--){
-    str += " "
+  for(let spaces = height - rows; spaces>=1; spaces--){
+    str += "."
   }
   for (let num = rows;num>=0;num--){
     str += "#"
   }  
+
+  let para = document.createElement("p");
+  let node = document.createTextNode(str);
+  para.appendChild(node);
+  let element = document.getElementById("pyramid");
+  element.appendChild(para);
+
   return str
 }
 
 for(let i = 1; i <= height;i++){
   console.log(printPyramid(i, height))
 }
+
+
